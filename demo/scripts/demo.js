6a43e15c8d3e3b362ee05ddc9c1b07a216457aed
circleci <rosswarren4@gmail.com>
2015-06-09 12:48:23 +0100

    Bower Release

--- demo/scripts/demo.js
+++ demo/scripts/demo.js
@@ -1,2 +1,5 @@
 //if you need to intialise anything to get the demo to work...
+
+require('polyfill/src/scripts/polyfill');
+
 var local = {}; local['gradients'] = require('../../src/scripts/gradients.js');
